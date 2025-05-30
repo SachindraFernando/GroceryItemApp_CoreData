795025aaccbdb09074d9dc3fbc083312e00fdd96
Sachindra Fernando <sachindrafernando3@gmail.com>
2025-05-30 22:13:39 +0100

    Initial Commit

---
+++ GroceryAppCoreData/GroceryAppCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppCoreDataApp.swift
+//  GroceryAppCoreData
+//
+//  Created by Sachindra Fernando on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
